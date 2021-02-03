6d6ac3c80e0265dc4b026e806590c36ab5b68c73
NadiaKH <Nadianhasan@gmail.com>
2021-02-03 12:42:21 +0300

    Create test_1.py

---
+++ task/test_1.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_1():
+    names = getUniqueNames('data')
+    graph = dict(key_val for key_val in [(name, []) for name in names])
+    buildGraph(graph, 'data')
+    
+    prev_g = None
+    for g in GraphIterator(graph):
+        if prev_g:
+            assert prev_g[1] >= g[1]
+        prev_g = g
